6bde67cc0e0a1b4e36c4543e9e83ebf173424a3e
David Tuckett <dtuc0357@uni.sydney.edu.au>
2021-07-26 12:01:05 +1000

    NaiveDecoder: move inner constructor to outer

--- test/models/generic/naivedecoder.jl
+++ test/models/generic/naivedecoder.jl
@@ -24,6 +24,6 @@ using Qecsim.PauliTools:bsp, to_bsf
     error = to_bsf("IIIIIII")
     syndrome = bsp(stabilizers(code), error)
     @test_throws ArgumentError decode(NaiveDecoder(5), code, syndrome)  # max_qubits=5
-    @test decode(NaiveDecoder(7), code, syndrome).recovery == error  # max_qubits=7
+    @test decode(NaiveDecoder(), code, syndrome).recovery == error  # max_qubits=10
     @test decode(NaiveDecoder(-1), code, syndrome).recovery == error  # max_qubits=unlimited
 end
